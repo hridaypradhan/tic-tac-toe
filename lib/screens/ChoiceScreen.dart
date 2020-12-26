f3e4f0fcaea6685cba657c7ded5aac5d07e6f1c6
Hriday Pradhan <hridayjyotipradhandc@gmail.com>
2020-12-26 23:48:43 +0530

    Started editing join a game functionality

--- lib/screens/ChoiceScreen.dart
+++ lib/screens/ChoiceScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/components/ButtonTemplate.dart';
 import 'package:flutter_app/constants.dart';
 import 'package:flutter_app/screens/NewGameScreen.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class ChoiceScreen extends StatefulWidget {
   static const String id = 'choice_screen';
@@ -11,6 +12,11 @@ class ChoiceScreen extends StatefulWidget {
 }
 
 class _ChoiceScreenState extends State<ChoiceScreen> {
+  int _digit1 = 0;
+  int _digit2 = 0;
+  int _digit3 = 0;
+  int _digit4 = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,17 +44,69 @@ class _ChoiceScreenState extends State<ChoiceScreen> {
                 showModalBottomSheet(
                   backgroundColor: Colors.transparent,
                   context: context,
-                  builder: (context) => Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(25.0),
-                    ),
-                    margin: EdgeInsets.only(top: 50.0),
-                    padding: EdgeInsets.all(25.0),
-                    child: Column(
-                      children: [
-                        Text('Hello'),
-                      ],
+                  builder: (context) => StatefulBuilder(
+                    builder: (context, setState) => Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25.0),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(Icons.drag_handle),
+                          Flexible(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                NumberPicker.integer(
+                                  infiniteLoop: true,
+                                  initialValue: _digit1,
+                                  minValue: 0,
+                                  maxValue: 9,
+                                  onChanged: (num value) {
+                                    setState(
+                                      () => _digit1 = value,
+                                    );
+                                  },
+                                ),
+                                NumberPicker.integer(
+                                  infiniteLoop: true,
+                                  initialValue: _digit2,
+                                  minValue: 0,
+                                  maxValue: 9,
+                                  onChanged: (num value) {
+                                    setState(
+                                      () => _digit2 = value,
+                                    );
+                                  },
+                                ),
+                                NumberPicker.integer(
+                                  infiniteLoop: true,
+                                  initialValue: _digit3,
+                                  minValue: 0,
+                                  maxValue: 9,
+                                  onChanged: (num value) {
+                                    setState(
+                                      () => _digit3 = value,
+                                    );
+                                  },
+                                ),
+                                NumberPicker.integer(
+                                  infiniteLoop: true,
+                                  initialValue: _digit4,
+                                  minValue: 0,
+                                  maxValue: 9,
+                                  onChanged: (num value) {
+                                    setState(
+                                      () => _digit4 = value,
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
